a4b5ea120cbcd98b2b5a64919e4a0f81661f5ea5
Prasahant Verma <v-prverma@expedia.com>
2020-11-06 18:05:21 +0530

    preparing for testing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import React from "react";
+import React  from "react";
 import "./App.scss";
-import { observer, useLocalObservable} from "mobx-react";
 
-const App = observer(() => {
-  const store = useLocalObservable({
-    count: 1,
-    addOne() {
-      store.count++;
-    },
-    subtractOne() {
-      store.count--;
-    },
-  });
-  function addOneHandle() {
-    store.addOne();
-  }
 
-  function subtractOneHandle() {
-    store.subtractOne();
-  }
-
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Count : {store.count}</h1>
-        <button onClick={addOneHandle}>Add One</button>
-        <button onClick={subtractOneHandle}>Subtrack One</button>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          
+
+
+          </header>
+      </div>
   );
-});
+}
 
 export default App;
