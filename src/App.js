c51a72469706aca513b5701ab70e3703987be610
Prasahant Verma <v-prverma@expedia.com>
2020-11-28 11:55:59 +0530

    more fixes before tic tac toe game

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React  from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Link} from "react-router-dom";
 import TicTacToeHome from "./ticTacToe/Home";
+import HomePage from "./HomePage";
 
 const App = () =>{
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-         <h1>Welcome Home Page</h1>
-         <Route path="/ticTacToe" exact component={TicTacToeHome}></Route>
+        <Route path="/" exact component={HomePage}></Route>
+  
+        <Route path="/ticTacToe" exact component={TicTacToeHome}></Route>
          <h3><Link to="/ticTacToe" className='title'> Go To Tic Tage Toe Game</Link></h3>
         </header>
       </div>
