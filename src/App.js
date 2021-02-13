2943db2138380bce78741233fe7ca2a5993c8581
Prasahant Verma <v-prverma@expedia.com>
2021-02-13 21:36:24 +0530

    adding compont

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React  from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Link} from "react-router-dom";
 import draggableListHome from "./draggableList/home";
+import componentsDemo from "./components/demo";
 import HomePage from "./HomePage";
 
 const App = () =>{
@@ -13,6 +14,9 @@ const App = () =>{
   
         <Route path="/draggableList" exact component={draggableListHome}></Route>
         <h3><Link to="/draggableList" className='title'> Go to Draggable List</Link></h3> 
+
+        <Route path="/compoentsDemo" exact component={componentsDemo}></Route>
+        <h3><Link to="/compoentsDemo" className='title'> Go to Components Demo</Link></h3> 
        </header>
       </div>
     </BrowserRouter>  
--- src/draggableList/home.js
+++ src/draggableList/home.js
@@ -1,4 +1,4 @@
-import React, { useState, userEffect } from "react";
+import React, { useState} from "react";
 import { Link } from "react-router-dom";
 
 const initList = [1, 2, 3, 4, 5, 6];
