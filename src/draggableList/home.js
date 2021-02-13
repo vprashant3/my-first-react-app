c3385662e67a87789ea6ec19b1446a88074b983e
Prasahant Verma <v-prverma@expedia.com>
2021-02-13 15:59:18 +0530

    draggable list complete

--- src/draggableList/home.js
+++ src/draggableList/home.js
@@ -1,8 +1,30 @@
 import React, { useState, userEffect } from "react";
 import { Link } from "react-router-dom";
 
-
+const initList = [1, 2, 3, 4, 5, 6];
 function HomePage() {
+  const [list, setList] = useState(initList);
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  function onDragStartHandle(e, idx) {
+    setDraggedItem(list[idx]);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode,20,20);
+  }
+
+  function onDragOverHandle(idx) {
+    const draggedOverItem = list[idx];
+    if(draggedOverItem === draggedItem) {
+      return;
+    }
+    const items  = list.filter(item => item !== draggedItem);
+
+    items.splice(idx, 0, draggedItem);
+
+    setList(items);
+
+  }
   return (
     <div>
       <h1>Draggable List Home</h1>
@@ -11,6 +33,12 @@ function HomePage() {
           Go To Home
         </Link>
       </h3>
+      <h3>Drag and Drop List</h3>
+      <ul>
+        {list.map((item, idx) => {
+          return <li key={idx} className="item-style"><div draggable onDragOver={() => onDragOverHandle(idx)} onDragStart={(e) => onDragStartHandle(e, idx)}>{item}</div></li>;
+        })}
+      </ul>
       <div></div>
     </div>
   );
